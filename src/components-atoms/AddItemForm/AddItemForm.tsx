4a7b90ab1f293f602e9bffeb220e945aebd8213c
sarahmhd <sarahmhd213199@gmail.com>
2024-06-27 02:33:00 +0300

    translate add button

--- src/components-atoms/AddItemForm/AddItemForm.tsx
+++ src/components-atoms/AddItemForm/AddItemForm.tsx
@@ -26,7 +26,7 @@ const AddItemForm = () => {
   return (
     <form onSubmit={handleSubmit} className='flex relative w-full'>
       <input className='w-full focus:outline-none' type="text" placeholder={t("inputPlaceholder")} value={item.value} onChange={handleChange} />
-      <button type='submit' className={`absolute top-0 h-full w-28 bg-[#cda1a7] text-[#fff] uppercase ${i18n.language === 'en' ? 'right-0' : 'left-0'}`}>add</button>
+      <button type='submit' className={`absolute top-0 h-full w-28 bg-[#cda1a7] text-[#fff] uppercase ${i18n.language === 'en' ? 'right-0' : 'left-0'}`}>{t('add')}</button>
     </form>
   );
 }
